308215eeedb7ffeac9b301df657ec4baa9aa78e3
Krisztian <krisztian.valyi1@gmail.com>
2023-04-12 16:10:31 +0100

    added cors

--- backend/src/main/java/com/fscalculator/demo/CalculationController.java
+++ backend/src/main/java/com/fscalculator/demo/CalculationController.java
@@ -4,6 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,11 +12,13 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/calculations")
+@CrossOrigin(origins = "*")
 public class CalculationController {
     @Autowired
     private CalculationService calculationService;
     @Autowired
     private CalculationRepository calculationRepository;
+
     @GetMapping
     public ResponseEntity<List<Calculation>> getAllCalculations(){
         return new ResponseEntity<List<Calculation>>(calculationService.AllCalculations(), HttpStatus.OK);
--- backend/src/main/java/com/fscalculator/demo/DemoApplication.java
+++ backend/src/main/java/com/fscalculator/demo/DemoApplication.java
@@ -2,18 +2,27 @@ package com.fscalculator.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping("/")
-	public String apiRoot(){
-		return "Hello world";
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 }
+
